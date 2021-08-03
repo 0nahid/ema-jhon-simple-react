e44590dae4ad9eb78f9f86b4c5741080952bd298
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-08-03 18:29:08 +0600

    added nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,12 @@ import './Header.css'
 const Header = () => {
     return (
         <div className="header">
-            <img src={logo} alt="" />
+            <img src={logo} alt="Ema John Son" />
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/reviews">Order Review</a>
+                <a href="/manage">Manage Inventory</a>
+            </nav>
         </div>
     );
 };
