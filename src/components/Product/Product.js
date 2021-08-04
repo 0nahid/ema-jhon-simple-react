618e965f499b1ab608909c24dd204d030a1ceb67
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-08-04 21:39:16 +0600

    adeed react icon

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './Product.css'
+import { FaShoppingCart } from "react-icons/fa";
 const Product = (props) => {
-    const { img, name ,url } = props.product;
+    const { img, name, url, stock, seller, price } = props.product;
     console.log(props);
     return (
         <div className="product">
             <div>
-            <a href={url}> <img src={img} alt="" /> </a>
+                <a href={url}> <img src={img} alt="" /> </a>
             </div>
             <div className="product-detail">
                 <h2>{name}</h2>
+                <p><small>By:{seller}</small></p>
+                <p>Price:${price}</p>
+                <p>Only {stock} left - order quickly.</p>
+                <button className="cart-button"> <FaShoppingCart/>Add to cart</button>
             </div>
         </div>
     );
 };
-
 export default Product;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,7 +4,8 @@ import fakeData from '../../fakeData'
 import Product from '../Product/Product';
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
-    const [products, setProducts] = useState(first10)
+    const [products, setProducts] = useState(first10);
+    
     return (
         <div className="shop-container">
             <div className="product-container">
