620dae54abdcf4b6a26357f6a6da7e1c0cc587ce
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-08-05 14:26:28 +0600

    added css root '

--- src/App.js
+++ src/App.js
@@ -6,11 +6,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <Me></Me> */}
         <Header></Header>
         <Shop></Shop>
       </header>
     </div>
   );
 }
-
+// function Me() {
+//   return (
+//     <div>
+//       <h1 style={{color: 'var(--main-color)'}}>Hello World</h1>
+//     </div>
+//   )
+// }
 export default App;
